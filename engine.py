bfdb1ef6cc6172e1b1742b61fc4e1273833f917a
chihab_eddine98 <ga_benamara@esi.dz>
2021-06-09 15:11:10 +0200

    NNUE evaluation setup

--- engine.py
+++ engine.py
@@ -1,13 +1,21 @@
 import chess
+import chess.engine
+from stockfish import Stockfish
 
 
 
 if __name__ == '__main__':
-    board = chess.Board()
+    engine = chess.engine.SimpleEngine.popen_uci('stockfish')
+    sf = Stockfish('/opt/homebrew/bin/stockfish')
+    sf.set_fen_position("rnbk3r/ppp3pp/8/7n/3b4/2P5/PP1K1P1P/RNB4q b - -")
+    print(sf.get_evaluation())
+
+    board = chess.Board("rnbk3r/ppp3pp/8/7n/3b4/2P5/PP1K1P1P/RNB4q b - -")
+    info = engine.analyse(board, chess.engine.Limit(depth=10), game=object())
+    print(info["score"])
+
 
     print(board.unicode().replace('⭘','.'))
-    board.push_san('e4')
-    print(board.unicode().replace('⭘','.'))
-    board.push_san('e5')
-    print(board.unicode().replace('⭘','.'))
+
+
 
